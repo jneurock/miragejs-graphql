c8b4a3a3bfade4d8c675b7ad12c5e858f81dc227
Rocky Neurock <jneurock@gmail.com>
2020-06-25 22:52:30 +0200

    Add more utils tests

--- __tests__/unit/utils-test.js
+++ __tests__/unit/utils-test.js
@@ -1,7 +1,47 @@
+import { GraphQLObjectType, GraphQLScalarType } from "graphql";
+import gql from "graphql-tag";
 import { setTypename } from "@lib/utils";
+import { ensureExecutableGraphQLSchema, unwrapInterfaceType } from "../../lib/utils";
 
 describe("Unit | utils", function () {
-  // TODO: Do we need a unit test for this?
+  describe("ensure exectuable GraphQL schema", function () {
+    function testSchema(schema) {
+      const exectuableSchema = ensureExecutableGraphQLSchema(schema);
+      const typeMap = exectuableSchema.getTypeMap();
+      const { bar } = typeMap.Foo.getFields();
+      const { foo } = typeMap.Query.getFields();
+
+      expect(bar.name).toBe("bar");
+      expect(bar.type).toBeInstanceOf(GraphQLScalarType);
+      expect(foo.name).toBe("foo");
+      expect(foo.type).toBeInstanceOf(GraphQLObjectType);
+    }
+
+    test("if the schema is a string", function () {
+      testSchema(`
+      type Foo {
+        bar: String
+      }
+
+      type Query {
+        foo: Foo
+      }
+      `);
+    });
+
+    test("if the schema is an AST", function () {
+      testSchema(gql`
+      type Foo {
+        bar: String
+      }
+
+      type Query {
+        foo: Foo
+      }
+      `);
+    });
+  });
+
   it("can set the __typename of an object", function () {
     const obj = { foo: "bar" };
 
@@ -10,9 +50,25 @@ describe("Unit | utils", function () {
     expect(obj).toEqual({ foo: "bar", __typename: "Foo" });
   });
 
+  it("can unwrap an interface type based on resolver info", function () {
+    const Foo = {};
+    const selection = {
+      kind: "InlineFragment",
+      typeCondition: { name: { value: "Foo" } },
+    };
+    const info = {
+      fieldNodes: [{ selectionSet: { selections: [selection] } }],
+      schema: {
+        getTypeMap: () => ({ Foo })
+      },
+    };
+
+    expect(unwrapInterfaceType(info)).toEqual(Foo);
+  });
+
   /**
    * TODO:
-   *   - test `unwrapInterfaceType`
    *   - test `unwrapType`
+   *   - test `unwrapTypeForModels`
    */
 });
