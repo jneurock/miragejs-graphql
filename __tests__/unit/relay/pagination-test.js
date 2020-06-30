78a666daedd61db2a7f085b7a3c9654d6e5d66ba
Rocky Neurock <jneurock@gmail.com>
2020-06-30 22:03:27 +0200

    Refactor and test Relay resolvers and utils and some imports

--- __tests__/unit/relay/pagination-test.js
+++ __tests__/unit/relay/pagination-test.js
@@ -1,11 +1,14 @@
+import { createConnection, encode, createRecords } from "./test-support";
 import {
   getRelayArgs,
   isRelayConnectionType,
   isRelayEdgeType,
   isRelayType,
-} from "@lib/relay/pagination";
+  setEdges,
+  setPageInfo,
+} from "@lib/relay-pagination";
 
-describe("Unit | Relay | pagination", function () {
+describe("Unit | Relay pagination", function () {
   const connectionType = {
     name: "RelayConnection",
     getFields: () => ({ edges: [] }),
@@ -35,7 +38,7 @@ describe("Unit | Relay | pagination", function () {
 
     test("if non Relay type", function () {
       const nonRelayType = { name: "Foo" };
-      const otherNonRelayType = { name: "Connection", getFields: () => ({})};
+      const otherNonRelayType = { name: "Connection", getFields: () => ({}) };
 
       expect(isRelayType(nonRelayType)).toBe(false);
       expect(isRelayType(otherNonRelayType)).toBe(false);
@@ -68,6 +71,177 @@ describe("Unit | Relay | pagination", function () {
       last: 10,
       after: "12345",
       before: "54321",
+    });
+  });
+
+  describe("set page info of a connection", function () {
+    test("when previous page", function () {
+      const connection = createConnection({
+        edges: [
+          { cursor: 11, node: { id: 11 } },
+          { cursor: 12, node: { id: 12 } },
+        ],
+      });
+      const records = [{ id: 1 }, { id: 12 }];
+
+      setPageInfo(connection, records);
+
+      expect(connection.pageInfo).toEqual({
+        hasPreviousPage: true,
+        hasNextPage: false,
+        startCursor: 11,
+        endCursor: 12,
+      });
+    });
+
+    test("when next page", function () {
+      const connection = createConnection({
+        edges: [
+          { cursor: 1, node: { id: 1 } },
+          { cursor: 2, node: { id: 2 } },
+        ],
+      });
+      const records = [{ id: 1 }, { id: 3 }];
+
+      setPageInfo(connection, records);
+
+      expect(connection.pageInfo).toEqual({
+        hasPreviousPage: false,
+        hasNextPage: true,
+        startCursor: 1,
+        endCursor: 2,
+      });
+    });
+
+    test("when both previous and next page", function () {
+      const connection = createConnection({
+        edges: [
+          { cursor: 2, node: { id: 2 } },
+          { cursor: 3, node: { id: 3 } },
+        ],
+      });
+      const records = [{ id: 1 }, { id: 4 }];
+
+      setPageInfo(connection, records);
+
+      expect(connection.pageInfo).toEqual({
+        hasPreviousPage: true,
+        hasNextPage: true,
+        startCursor: 2,
+        endCursor: 3,
+      });
+    });
+
+    test("when neither previous or next page", function () {
+      const connection = createConnection({
+        edges: [
+          { cursor: 1, node: { id: 1 } },
+          { cursor: 2, node: { id: 2 } },
+        ],
+      });
+      const records = [{ id: 1 }, { id: 2 }];
+
+      setPageInfo(connection, records);
+
+      expect(connection.pageInfo).toEqual({
+        hasPreviousPage: false,
+        hasNextPage: false,
+        startCursor: 1,
+        endCursor: 2,
+      });
+    });
+  });
+
+  describe("set edges of a connection", function () {
+    test("with no args", function () {
+      const args = {};
+      const connection = createConnection();
+      const records = createRecords(1);
+
+      setEdges(connection, records, args, "Foo", encode);
+
+      expect(connection.edges).toEqual([
+        {
+          cursor: "Foo:1",
+          node: { id: "1", foo: "bar1" },
+        },
+      ]);
+    });
+
+    test("with first/after args", function () {
+      const args = { first: 2, after: "Foo:2" };
+      const connection = createConnection();
+      const records = createRecords(5);
+
+      setEdges(connection, records, args, "Foo", encode);
+
+      expect(connection.edges).toEqual([
+        {
+          cursor: "Foo:3",
+          node: { id: "3", foo: "bar3" },
+        },
+        {
+          cursor: "Foo:4",
+          node: { id: "4", foo: "bar4" },
+        },
+      ]);
+    });
+
+    test("with last/before args", function () {
+      const args = { last: 2, before: "Foo:4" };
+      const connection = createConnection();
+      const records = createRecords(5);
+
+      setEdges(connection, records, args, "Foo", encode);
+
+      expect(connection.edges).toEqual([
+        {
+          cursor: "Foo:2",
+          node: { id: "2", foo: "bar2" },
+        },
+        {
+          cursor: "Foo:3",
+          node: { id: "3", foo: "bar3" },
+        },
+      ]);
+    });
+
+    test("with first/before args", function () {
+      const args = { first: 3, before: "Foo:3" };
+      const connection = createConnection();
+      const records = createRecords(5);
+
+      setEdges(connection, records, args, "Foo", encode);
+
+      expect(connection.edges).toEqual([
+        {
+          cursor: "Foo:1",
+          node: { id: "1", foo: "bar1" },
+        },
+        {
+          cursor: "Foo:2",
+          node: { id: "2", foo: "bar2" },
+        },
+      ]);
+    });
+
+    test("with last/after args", function () {
+      const args = { last: 3, after: "Foo:3" };
+      const connection = createConnection();
+      const records = createRecords(5);
+
+      setEdges(connection, records, args, "Foo", encode);
+
+      expect(connection.edges).toEqual([
+        {
+          cursor: "Foo:4",
+          node: { id: "4", foo: "bar4" },
+        },
+        {
+          cursor: "Foo:5",
+          node: { id: "5", foo: "bar5" },
+        },
+      ]);
     });
   });
 });
